6560ee914af5508a89e3a57a6bb36a2eec942af0
dklmn <dk@geoquant.com>
2019-06-19 23:17:31 +0300

    Commenting out the indexing steps so now it just launches using the persistent disk

--- launch.sh
+++ launch.sh
@@ -1,34 +1,34 @@
-cd conf
-rm tomcat-users.xml
-wget https://raw.githubusercontent.com/ahalterman/CLIFF-up/master/tomcat-users.xml
-$CATALINA_HOME/bin/startup.sh
+# cd conf
+# rm tomcat-users.xml
+# wget https://raw.githubusercontent.com/ahalterman/CLIFF-up/master/tomcat-users.xml
+# $CATALINA_HOME/bin/startup.sh
 
-echo "Getting CLIFF..."
-echo "  downloading Cliff WAR file from GitHub"
-cd; wget https://github.com/mitmedialab/CLIFF/releases/download/v2.4.2/cliff-2.4.2.war
-mv cliff-2.4.2.war /usr/local/tomcat/webapps/
-echo "  done (copied /usr/local/tomcat/webapps/)"
+# echo "Getting CLIFF..."
+# echo "  downloading Cliff WAR file from GitHub"
+# cd; wget https://github.com/mitmedialab/CLIFF/releases/download/v2.4.2/cliff-2.4.2.war
+# mv cliff-2.4.2.war /usr/local/tomcat/webapps/
+# echo "  done (copied /usr/local/tomcat/webapps/)"
 
-echo "Building Index..."
-echo "  downloading CLAVIN from GitHub"
-cd; git clone https://github.com/Berico-Technologies/CLAVIN.git
+# echo "Building Index..."
+# echo "  downloading CLAVIN from GitHub"
+# cd; git clone https://github.com/Berico-Technologies/CLAVIN.git
 
-echo "  downloading latestplacenames file from Geonames"
-cd CLAVIN
-wget http://download.geonames.org/export/dump/allCountries.zip
-unzip allCountries.zip
-rm allCountries.zip
+# echo "  downloading latestplacenames file from Geonames"
+# cd CLAVIN
+# wget http://download.geonames.org/export/dump/allCountries.zip
+# unzip allCountries.zip
+# rm allCountries.zip
 
-echo "  compiling CLAVIN"
-mvn compile
+# echo "  compiling CLAVIN"
+# mvn compile
 
-echo "  building Lucene index of placenames (takes up to 20 mins)"
-MAVEN_OPTS="-Xmx4g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder"
+# echo "  building Lucene index of placenames (takes up to 20 mins)"
+# MAVEN_OPTS="-Xmx4g" mvn exec:java -Dexec.mainClass="com.bericotech.clavin.index.IndexDirectoryBuilder"
 
-echo "  symlink /etc/cliff2 to the new IndexDirectory that was just built"
-mkdir /etc/cliff2
-cp -r IndexDirectory /etc/cliff2/IndexDirectory
-echo " done"
+# echo "  symlink /etc/cliff2 to the new IndexDirectory that was just built"
+# mkdir /etc/cliff2
+# cp -r IndexDirectory /etc/cliff2/IndexDirectory
+# echo " done"
 
 echo "Start up Tomcat..."
 $CATALINA_HOME/bin/shutdown.sh
